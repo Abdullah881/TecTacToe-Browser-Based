a8fc592393ab4898e7647b3f2f4fe3f9f153ec52
Abdullah881 <abdullah.altaf881@gmail.com>
2023-09-08 13:14:32 +0500

    Check Win Function added

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -52,3 +52,64 @@ function placeXOrO(squareNumber) {
         }
     }
 }
+
+//This Function parses the selectedSquares array to search for win conditions
+// draw line() function is called to draw a line on the screen if the condition is met
+
+function checkWinConditions(){
+    // X @ 0,1,2
+    if (arrayIncludes('0X','1X','2X')){ drawWinLine(50,100,558,100)}
+    // X @ 3,4,5
+    else if (arrayIncludes('3X','4X','5X')){ drawWinLine(50,304,558,304)}
+    // X @ 6,7,8
+    else if (arrayIncludes('6X','7X','8X')){ drawWinLine(50,508,558,508)}
+    // X @ 0,3,6
+    else if (arrayIncludes('0X','3X','6X')){ drawWinLine(100,50,100,558)}
+    // X @ 1,4,7
+    else if (arrayIncludes('1X','4X','7X')){ drawWinLine(304,50,304,558)}
+    // X @ 2,5,8
+    else if (arrayIncludes('2X','5X','8X')){ drawWinLine(508,50,508,558)}
+    // X @ 6,4,2
+    else if (arrayIncludes('6X','4X','2X')){ drawWinLine(100,508,510,90)}
+    // X @ 0,4,8
+    else if (arrayIncludes('0X','4X','8X')){ drawWinLine(100,100,520,520)}
+    // O @ 0,1,2
+    else if (arrayIncludes('0O','1O','2O')){ drawWinLine(50,100,558,100)}
+    // O @ 3,4,5
+    else if (arrayIncludes('3O','4O','5O')){ drawWinLine(50,304,558,304)}
+    // O @ 6,7,8
+    else if (arrayIncludes('6O','7O','8O')){ drawWinLine(50,508,558,508)}
+    // O @ 0,3,6
+    else if (arrayIncludes('0O','3O','6O')){ drawWinLine(100,50,100,558)}
+    // O @ 1,4,7
+    else if (arrayIncludes('1O','4O','7O')){ drawWinLine(304,50,304,558)}
+    // O @ 2,5,8
+    else if (arrayIncludes('2O','5O','8O')){ drawWinLine(508,50,508,558)}
+    // O @ 6,4,2
+    else if (arrayIncludes('6O','4O','2O')){ drawWinLine(100,508,510,90)}
+    // O @ 0,4,8
+    else if (arrayIncludes('0O','4O','8O')){ drawWinLine(100,100,520,520)}
+    // this condition checks for a tie. If none of the above conditions are met and
+    //9 out of 9 squares have been filles
+    else if (selectedSquares.length >= 9) {
+        // play tie sound with audio() function
+        audio('./media/tie.mp3')
+        // reset game after 1 second
+        setTimeout(function() {resetGame();},1000);
+    }
+
+    // this function checks if an array includes 3 strings. It is used to check for each win condition
+    function arrayIncludes(squareA,squareB,squareC) {
+        // these 3 variables will be used to check for 3 in a row
+        const a = selectedSquares.includes(squareA);
+        const b = selectedSquares.includes(squareB);
+        const c = selectedSquares.includes(squareC);
+
+        // if all three are in included in our array then
+        // true is returned and draw line() function is executes
+
+        if (a==true && b == true && c == true) {return true;}
+
+    }
+
+}
